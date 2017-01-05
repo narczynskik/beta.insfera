731cc9a6c90745553290985b19708ed98f407037
narczynskik <narczynski.krzysztof@gmail.com>
2017-01-05 12:39:18 +0100

    dodanie podstrony kontakt,i ustawienie google maps oraz plikow wideo

--- js/reactions.js
+++ js/reactions.js
@@ -12,6 +12,19 @@
             $("#insferaDefinition").slideUp();
             $("#expandInsferaDefinition").show();
         });
+
+
+         $("#expandInsfera1Definition").click(function(){
+            $("#expandInsfera1Definition").hide();
+            $("#insfera1Definition").slideDown();
+            $("#hideInsfera1Definition").show();
+        });
+        
+        $("#hideInsfera1Definition").click(function(){
+            $("#hideInsfera1Definition").hide();
+            $("#insfera1Definition").slideUp();
+            $("#expandInsfera1Definition").show();
+        });
         
         $("#more-btn1").click(function(){
             $("#def-window1").show();
