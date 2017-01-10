f5ddc877376f6bb635925ecce64892bd058221e3
narczynskik <narczynski.krzysztof@gmail.com>
2017-01-10 13:29:09 +0100

    ostylowanie formularza

--- mail.php
+++ mail.php
@@ -6,5 +6,5 @@ $headers = 'From: ' . $_POST['email'] . "\r\n" .
     'Content-type: text/html; charset=utf-8';
 
 mail($to, $subject, $message, $headers);
-echo 'Wiadomość została wysłana';
+echo 'Wiadomość została wysłana'
 ?>
