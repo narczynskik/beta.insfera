40e6d872fd2aef6dc90326aebaf24d72a195107f
narczynskik <narczynski.krzysztof@gmail.com>
2017-01-10 12:31:14 +0100

    dodanie panelu wysylania wiadomosci + php

---
+++ mail.php
@@ -0,0 +1,10 @@
+<?php
+$to      = 'narczynski@gmail.com';
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+$headers = 'From: ' . $_POST['email'] . "\r\n" .
+    'Content-type: text/html; charset=utf-8';
+
+mail($to, $subject, $message, $headers);
+echo 'Wiadomość została wysłana';
+?>
